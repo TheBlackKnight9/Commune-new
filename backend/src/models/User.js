5bf6959dfba253e9c6c620a39d8fd3744fe9cf91
TheBlackKnight9 <codedemon27@gmail.com>
2025-06-19 14:05:58 +0530

    add FriendRequest model and implement password hashing

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,5 +1,5 @@
-import { type } from "express/lib/response";
 import mongoose  from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
     fullName: {
@@ -47,7 +47,6 @@ const userSchema = new mongoose.Schema({
     }],
 }, {timestamps: true});
 
-const User = mongoose.model("User", userSchema);
 
 //pre hook
 userSchema.pre("save", async function(next) {
@@ -55,11 +54,18 @@ userSchema.pre("save", async function(next) {
     try {
         const salt = await bcrypt.genSalt(10);
         this.password - await bcrypt.hash(this.password, salt);
-
+        
         next();
     } catch (error) {
-      next(error);
+        next(error);
     }
 });
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    const isPasswordCorrect = await bcrypt.compare(enteredPassword, this.password);
+    return isPasswordCorrect;
+};
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
