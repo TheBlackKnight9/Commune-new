da67271e36198f66c15204c48169bf9b8ab4b1b2
TheBlackKnight9 <codedemon27@gmail.com>
2025-06-19 09:38:46 +0530

    implement user model and signup functionality with validation and JWT token generation

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,44 @@
-export function signup(req, res) {
-  res.send('Signup Route');
+import User from "../models/User";
+import jwt from "jsonwebtoken";
+
+export async function signup(req, res) {
+  const {email, password, fullNmae } = req.body;
+
+  try {
+
+    if(!email || !password || !fullNmae) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    if(password.length < 6) {
+      return res.status(400).json({ message: 'Password must be at least 6 characters long' });
+    }
+
+    if(!email.includes("@")) {
+      return res.status(400).json({ message: 'Invalid email address' });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if(existingUser){
+        return res.status(400).json({ message: 'email already exists, please use different one' });
+    }
+
+    const idx = Math.floor(Math.random() * 100) + 1;
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+    const newUser = await User.create({
+      email,
+      password,
+      fullNmae,
+      profilePic: randomAvatar,
+    })
+
+    const token = jwt.sign({userId: newUser._id}, process.env.JWT_SECRET, {
+      expiresIn: '8d',
+    })
+    } catch (error){
+
+  }
 }
 
 export function login(req, res) {
